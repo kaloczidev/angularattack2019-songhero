31a957a63587a88db8ce4a1944f35e1e080e1d1c
Tamas Csaba <csaba.tamas@balabit.com>
2019-03-30 23:20:08 +0100

    add bad-words detect

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions';
 
 require = require('esm')(module);
 const RC4 = require('rc4-es6').default;
+const BadWords = require('bad-words');
 
 export const decrypt = functions
   .region('europe-west1')
@@ -9,12 +10,12 @@ export const decrypt = functions
   .document('leaderboard/{id}')
   .onWrite( change => {
     const value:any = change.after.data();
-    const rc4 = new RC4('you shall not pass');
-    const decryptedScore = rc4.decrypt(value.score);
+
+    const decryptedScore = new RC4('you shall not pass').decrypt(value.score);
     const validScore = Number(decryptedScore) ? Number(decryptedScore) : 0;
 
     return change.after.ref.set({
-      name: value.name,
+      name: new BadWords().clean(value.name),
       score: validScore
     }, {merge: true});
 });
