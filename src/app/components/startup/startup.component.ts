0ae2fd8d18e0579c9e3ae8b6e48e7cc05a7c1241
gergelyszabo <gergely.szabo@balabit.com>
2019-03-30 11:08:19 +0100

    startup screen

--- src/app/components/startup/startup.component.ts
+++ src/app/components/startup/startup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-startup',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./startup.component.scss']
 })
 export class StartupComponent implements OnInit {
+  @ViewChild('modal') modal: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  close() {
+    this.modal.nativeElement.className += ' close';
   }
 
 }
