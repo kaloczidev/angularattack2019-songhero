46324538c2715e65c19017be113b227f00aefccb
Zana László <laszlo@zana.hu>
2019-03-30 11:04:04 +0100

    svg map

--- src/app/components/control/control.component.ts
+++ src/app/components/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ControlService } from './control.service';
 import { DollService } from '../doll/doll.service';
 
@@ -7,22 +7,12 @@ import { DollService } from '../doll/doll.service';
   templateUrl: './control.component.html',
   styleUrls: ['./control.component.scss'],
 })
-export class ControlComponent implements OnInit {
-  @ViewChild('display') display: ElementRef<HTMLCanvasElement>;
-
-  private canvas: HTMLCanvasElement;
-  private ctx: CanvasRenderingContext2D;
-
+export class ControlComponent {
   private spaceKeyPressed = 0;
 
   constructor(private service: ControlService,
               private dollService: DollService
   ) {
-  }
-
-  ngOnInit(): void {
-    this.canvas = this.display.nativeElement;
-    this.ctx = this.canvas.getContext('2d');
   }
 
   @HostListener('window:keydown', ['$event'])
@@ -42,26 +32,7 @@ export class ControlComponent implements OnInit {
     }
   }
 
-  draw(map) {
-    // this.ctx.beginPath();
-    // this.ctx.moveTo(50, 20);
-    // this.ctx.quadraticCurveTo(60, 32, 70, 20);
-    // this.ctx.closePath();
-
-    // this.ctx.lineJoin = 'round';
-    // this.ctx.lineWidth = 6;
-
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    map.forEach((value, index) => {
-      if (value) {
-        this.ctx.rect(20, index * 10, 10, 10);
-      }
-    });
-
-    this.ctx.fillStyle = 'red';
-    // this.ctx.strokeStyle = 'red';
-    // this.ctx.stroke();
-    this.ctx.fill();
+  isHightlighted(x: number, y: number): boolean {
+    return x === 1 && y === 2;
   }
 }
