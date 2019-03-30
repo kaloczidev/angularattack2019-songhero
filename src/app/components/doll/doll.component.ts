e495fa44503522f5b5360cb91f0626c1ec7e2627
gergelyszabo <gergely.szabo@balabit.com>
2019-03-30 03:56:45 +0100

    soundcloud api init

--- src/app/components/doll/doll.component.ts
+++ src/app/components/doll/doll.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PlayerService} from '../../services/player/player.service';
 
 @Component({
   selector: 'app-doll',
@@ -7,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DollComponent implements OnInit {
 
-  constructor() {
+  songs = [
+    {
+      name: 'Robin Schulz - Sun Goes Down',
+      trackId: '167810531'
+    },
+    {
+      name: 'Lost Frequencies - Are you with me',
+      trackId: '125851434'
+    },
+    {
+      name: 'Random',
+      trackId: '208277289'
+    },
+    {
+      name: 'WhatIsLove',
+      trackId: '86589991'
+    }
+  ];
+
+  constructor(private player: PlayerService) {
   }
 
   ngOnInit() {
+    this.player.play(this.songs[1].trackId);
   }
 
 }
