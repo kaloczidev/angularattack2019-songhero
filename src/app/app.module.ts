2e2ba57bd3bdf6b8bf0aa2d02c2461dd9efa1b0b
Zana László <laszlo@zana.hu>
2019-03-30 02:08:21 +0100

    init doll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DollComponent } from './components/doll/doll.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DollComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +17,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/doll/doll.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doll',
+  templateUrl: './doll.component.html',
+  styleUrls: ['./doll.component.scss']
+})
+export class DollComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
