49b181c58f321a5a2d69553f0c02942a186f9ddf
Zana László <laszlo@zana.hu>
2019-03-30 17:12:28 +0100

    kan-vas

--- src/app/components/control/control.component.ts
+++ src/app/components/control/control.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { ControlService } from './control.service';
 import { DollService } from '../doll/doll.service';
 import { BitValuesUtil } from '../../utils/bitValues.util';
@@ -12,15 +12,20 @@ const whatIsLoveBuffer: ArrayBuffer = require('./what-is-love.data');
   templateUrl: './control.component.html',
   styleUrls: ['./control.component.scss'],
 })
-export class ControlComponent {
+export class ControlComponent implements OnInit {
   yArray = Array.from(new Array(30), (val, index) => index * 20 + 20);
+  xArray = Array.from(new Array(4), (val, index) => index * 20 + 20);
+
+  @ViewChild('display') display: ElementRef<HTMLCanvasElement>;
+
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
 
   private subMap: Uint8Array;
   private spaceKeyPressed = 0;
 
   constructor(private service: ControlService,
-              private dollService: DollService,
-              private cdr: ChangeDetectorRef,
+              private dollService: DollService
   ) {
     this.subMap = new Uint8Array(4 * 30);
     this.subMap.fill(0);
@@ -28,8 +33,15 @@ export class ControlComponent {
     service.map = new Uint8Array(whatIsLoveBuffer);
     service.subMap.subscribe((subMap: Uint8Array) => {
       this.subMap = subMap;
-      this.cdr.detectChanges();
+      this.draw();
     });
+  }
+
+  ngOnInit(): void {
+    this.canvas = this.display.nativeElement;
+    this.canvas.width = 500;
+    this.canvas.height = 800;
+    this.ctx = this.canvas.getContext('2d');
   }
 
   @HostListener('window:keydown', ['$event'])
@@ -58,7 +70,20 @@ export class ControlComponent {
     }
   }
 
-  isHighlighted(x: number, y: number): boolean {
-    return BitValuesUtil.getBit(this.subMap[x], y);
+  private isHighlighted(x: number, y: number): boolean {
+    return BitValuesUtil.getBit(this.subMap[29 - x], y);
+  }
+
+  private draw() {
+    this.ctx.fillStyle = '#FC6E51';
+    this.yArray.forEach((y: number, xIndex: number) => {
+      this.xArray.forEach((x: number, yIndex: number) => {
+        if (this.isHighlighted(xIndex, yIndex)) {
+          this.ctx.fillRect(x, y, 10, 20);
+        } else {
+          this.ctx.clearRect(x, y, 10, 20);
+        }
+      });
+    });
   }
 }
