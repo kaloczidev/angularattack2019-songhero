df5d483001a1a8d059e808458b8a9fb64451a4fc
Zana László <laszlo@zana.hu>
2019-03-31 06:31:19 +0200

    fix score

--- src/app/components/recorder/recorder.component.ts
+++ src/app/components/recorder/recorder.component.ts
@@ -22,6 +22,8 @@ import { BitValuesUtil } from '../../utils/bitValues.util';
   template: 'RECORDER: Don\'t forget to remove!!!!',
 })
 export class RecorderComponent {
+  private previousIndex = 0;
+
   private spacePressed = 0;
   private arrowRightPressed = 0;
   private arrowLeftPressed = 0;
@@ -43,11 +45,21 @@ export class RecorderComponent {
 
     this.playerService.onPositionChanged.subscribe((trackPosition: TrackPosition) => {
       const index = this.recordedDataByteLength * trackPosition.relativePosition | 0;
-      this.recordedData[index] = BitValuesUtil.set([this.spacePressed, this.arrowRightPressed, this.arrowLeftPressed, this.keyWPressed]);
+
+      if (index === this.previousIndex) {
+        this.recordedData[index] = BitValuesUtil.set([this.spacePressed, this.arrowRightPressed, this.arrowLeftPressed, this.keyWPressed]);
+      } else {
+        for (let i = this.previousIndex; i < index; ++i) {
+          this.recordedData[i] = BitValuesUtil.set([this.spacePressed, this.arrowRightPressed, this.arrowLeftPressed, this.keyWPressed]);
+        }
+      }
+
+      this.previousIndex = index;
     });
 
     playerService.status.subscribe((status: PlayerStatus) => {
       if (status === PlayerStatus.PLAY) {
+        this.previousIndex = 0;
         this.recordedData.fill(0);
       } else if (status === PlayerStatus.FINISHED) {
         const blob = new Blob([this.recordedData.buffer], {type: 'application/octet-stream'});
