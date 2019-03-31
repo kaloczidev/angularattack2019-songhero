9211beca8107faae0287f95b7aaf200526513818
gergelyszabo <gergely.szabo@balabit.com>
2019-04-01 01:24:24 +0200

    jej

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -2,7 +2,6 @@ import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
 import {PlayerService, PlayerStatus, TrackPosition} from './player.service';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import * as MobileDetect from 'mobile-detect';
-import {debounceTime} from 'rxjs/operators';
 
 declare var SC: any;
 
@@ -20,6 +19,7 @@ export class PlayerComponent implements OnInit {
   md = new MobileDetect(window.navigator.userAgent);
   isMobile = this.md.mobile() || this.md.tablet() || this.md.phone();
   status = PlayerStatus.IDLE;
+  isSafari = (window as any).safari !== undefined;
 
   constructor(private player: PlayerService, private sanitizer: DomSanitizer, private zone: NgZone) {
   }
@@ -29,9 +29,14 @@ export class PlayerComponent implements OnInit {
       this.blur.nativeElement.focus();
       this.status = status;
       switch (status) {
-        case PlayerStatus.PLAY: this.play(); break;
-        case PlayerStatus.PAUSE: this.pause(); break;
-        default: break;
+        case PlayerStatus.PLAY:
+          this.play();
+          break;
+        case PlayerStatus.PAUSE:
+          this.pause();
+          break;
+        default:
+          break;
       }
     });
 
@@ -48,10 +53,13 @@ export class PlayerComponent implements OnInit {
   }
 
   setUrl(src: string) {
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(`${this.BASE_URL}${src}`);
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(`${this.BASE_URL}${src}${this.isSafari ? '?auto_play=true' : ''}`);
   }
 
   play() {
+    if (this.isSafari) {
+      this.setUrl('34743391');
+    }
     this.setVolume(100);
     this.player.seekTo(0);
     SC.Widget(this.audio.nativeElement).play();
@@ -68,20 +76,38 @@ export class PlayerComponent implements OnInit {
   onload() {
     SC.Widget(this.audio.nativeElement).unbind(SC.Widget.Events.PLAY_PROGRESS);
     SC.Widget(this.audio.nativeElement).unbind(SC.Widget.Events.FINISH);
+    SC.Widget(this.audio.nativeElement).unbind(SC.Widget.Events.PAUSE);
 
     SC.Widget(this.audio.nativeElement).bind(SC.Widget.Events.FINISH, (event: TrackPosition) => this.zone.run(() => {
       this.player.status.next(PlayerStatus.FINISHED);
     }));
 
-    SC.Widget(this.audio.nativeElement).bind(SC.Widget.Events.PLAY_PROGRESS, (event: TrackPosition) => this.zone.run(() => {
-      if (this.player.loading.getValue()) this.player.loading.next(false);
+    SC.Widget(this.audio.nativeElement).bind(SC.Widget.Events.PAUSE, (event: TrackPosition) => this.zone.run(() => {
+      if (this.isSafari) {
+        this.blur.nativeElement.focus();
+        SC.Widget(this.audio.nativeElement).play();
+      }
+    }));
 
-      if (this.status === PlayerStatus.PLAY) this.player.onPositionChanged.next(event);
-      else this.player.pause();
+    SC.Widget(this.audio.nativeElement).bind(SC.Widget.Events.PLAY_PROGRESS, (event: TrackPosition) => this.zone.run(() => {
+      if (this.player.loading.getValue()) {
+        this.player.loading.next(false);
+      }
+
+      if (this.status === PlayerStatus.PLAY) {
+        if (this.isSafari) {
+          this.setVolume(100);
+        }
+        this.player.onPositionChanged.next(event);
+      } else if (!this.isSafari) {
+        this.player.pause();
+      }
     }));
 
     this.setVolume(0);
     this.player.loading.next(true);
-    SC.Widget(this.audio.nativeElement).play();
+    if (!this.isSafari) {
+      SC.Widget(this.audio.nativeElement).play();
+    }
   }
 }
