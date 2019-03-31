63d0fcf2fa2644f921c5df2a6582b1dc7eb73e49
gergelyszabo <gergely.szabo@balabit.com>
2019-03-31 12:42:44 +0200

    startapmodal

--- src/app/components/startup/startup.component.ts
+++ src/app/components/startup/startup.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {ModalComponent} from '../modal/modal.component';
-import { AppService } from '../../services/app.service';
+import {AppService} from '../../services/app.service';
 
 @Component({
   selector: 'app-startup',
@@ -10,15 +10,33 @@ import { AppService } from '../../services/app.service';
 export class StartupComponent implements OnInit {
   @ViewChild('modal') modal: ModalComponent;
 
-  constructor(private appService: AppService) { }
+  tour: Array<string> = ['zsák', 'hugy', 'os', 'faleveles', 'ganaj'];
+  tourActual = 0;
+
+  constructor(private appService: AppService) {
+  }
 
   ngOnInit() {
-    if (this.appService.showStartUp.getValue()) this.modal.open();
+    if (this.appService.showStartUp.getValue()) {
+      this.modal.open();
+    }
   }
 
   onStart(showDialogAgain: boolean) {
     this.appService.showStartUp.next(!showDialogAgain);
     localStorage.setItem('startUp', JSON.stringify(!showDialogAgain));
     this.modal.close();
+  }
+
+  next() {
+    if (this.tourActual + 1 < this.tour.length) {
+      this.tourActual++;
+    }
+  }
+
+  prev() {
+    if (this.tourActual - 1 > -1) {
+      this.tourActual--;
+    }
   }
 }
